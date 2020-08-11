d1f5347d86cd25f69dfe4370afe0ed7cb07e9dcf
Rebeca <bekapedroso@gmail.com>
2020-08-11 08:37:54 -0300

    atividades três

--- atividade03/src/App.js
+++ atividade03/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Creat from './components/Creat'
+import Edit from './components/Edit'
+import List from './components/List'
+import Home from './components/Home'
+
+export default class App extends Component{
+  render(){
+    return(
+      <Router>
+        <div className='container'>
+          <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <Link to = {'/'} className='navbar-brand'>CRUD</Link>
+            <div className='collapse navbar-collapse' id = 'navbarSupportedContent'>
+              <ul className='navbar-nav mr-auto'>
+                <li>
+                  <Link to ={'/'} className='nav-link'>Home</Link>
+                </li>
+                <li>
+                  <Link to ={'/creat'} className='nav-link'>Creat</Link>
+                </li>
+                <li>
+                  <Link to ={'/list'} className='nav-link'>List</Link>
+                </li>
+              </ul>
+
+            </div>
+          </nav>
+
+          <h2>Projeto CRUD</h2> <br/>
+          <Switch>
+            <Route exact path='/' component = {Home}/>
+            <Route path='/creat' component = {Creat}/>
+            <Route path='/edit' component = {Edit}/>
+            <Route path='/list' component = {List}/>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
-
-export default App;

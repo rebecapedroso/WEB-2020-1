c155b251e9c9d79ee12bb824ffd03d8a79b30b13
Rebeca <bekapedroso@gmail.com>
2020-08-25 19:44:57 -0300

    atividade 05(parte01)

--- crud-express/routes/users.js
+++ crud-express/routes/users.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+router.get('/get', function(req, res, next) {
+  res.json(
+    {users: 
+      [
+        {name: 'Timmy'},
+        {name: 'Timmy'},
+        {name: 'Billy'}
+      ]
+    }
+    );
+});
+
+router.get('/getget', function(req, res, next) {
+  res.json(
+    {users: 
+      [
+        {name: 'Timmy'}
+      ]
+    }
+    );
 });
 
 module.exports = router;
